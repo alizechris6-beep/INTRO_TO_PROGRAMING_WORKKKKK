9e7ed746d7b3be5f801d3b34a336b69a2b3ff3fe
alizechris6-beep <alizechris6@gmail.com>
2026-01-20 17:55:45 -0500

    learning variables

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 print("Chris is the best at dragon ball legends")
+#using the pound/hashtag symbol is a way to do comments
+print('Hello, how are you today?')
+
+name= "Scooby" #variables unlike other languages don't have a type.
+
+#int, double, float, char, string, boolean
+age = 18
+temperature = 67.69
+
+#types of printing
+print("I am {} years old, and the temprtature is {} degrees".format(age, temperature))
+
+#imput
+friendName = input("whats your name?\n")
+print("the new friend name is ", friendName)
+
+fage = int(input("whats your favorite age?\n"))
+print(fage)
+
+tota = 52 + 78
+print(tota)
+
+agesTotal = fage + age
+print(agesTotal)
+
+modulusTotal = 5 % 2
+print(modulusTotal)
