810f2c65a74b0a9353e2b11b4a5d52b93f2a0f40
alizechris6-beep <alizechris6@gmail.com>
2026-01-26 00:04:07 -0500

    Variables assighnment

---
+++ Variables assighnment.py
@@ -0,0 +1,26 @@
+# Calculation 1: 5 multiplied by 6
+result1 = 5 * 6
+print("The answer to your query is", result1)
+
+# Calculation 2: 8 divided by 3
+result2 = 8 / 3
+print("The answer to your query is", result2)
+
+# Calculation 3: 8 divided by 3 without the remainder
+result3 = 8 // 3
+print("The answer to your query is", result3)
+
+# Calculation 4: 8 modulo 3 (remainder only)
+result4 = 8 % 3
+print("The answer to your query is", result4)
+
+# Calculation 5: 4200000 add 23090
+result5 = 4200000 + 23090
+print("The answer to your query is", result5)
+
+# Calculation 6: 4200000 subtract 7633
+result6 = 4200000 - 7633
+print("The answer to your query is", result6)
+
+# Print my name at the end
+print("Chris Alize")
