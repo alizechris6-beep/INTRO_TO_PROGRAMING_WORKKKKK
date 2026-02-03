58e7257d541141c37669f13aef49fb4c5dc4cacc
alizechris6-beep <alizechris6@gmail.com>
2026-02-03 16:34:11 -0500

    variablesss

--- Variables assighnment.py
+++ Variables assighnment.py
@@ -1,26 +1,20 @@
-# Calculation 1: 5 multiplied by 6
-result1 = 5 * 6
-print("The answer to your query is", result1)
+#That we want to multiply two numbers
+numMulti = int(input("Enter a number: "))
+numMulti2 = int(input("Enter another number: "))
+answerMulti = numMulti * numMulti2
+print ("The answer to your query is", answerMulti)
 
-# Calculation 2: 8 divided by 3
-result2 = 8 / 3
-print("The answer to your query is", result2)
+#That we want to divide two numbers
+numDiv = int(input("Enter a number: "))
+numDiv2 = int(input("Enter another number: "))
+answerDiv = numDiv / numDiv2
+print ("The answer to your query is", answerDiv)
 
-# Calculation 3: 8 divided by 3 without the remainder
-result3 = 8 // 3
-print("The answer to your query is", result3)
+#That we want to divide without remainder two numbers
+numNoRemain = int(input("Enter a number: "))
+numNoRemain2 = int(input("Enter another number: "))
+answerNoRemain = numNoRemain // numNoRemain2
 
-# Calculation 4: 8 modulo 3 (remainder only)
-result4 = 8 % 3
-print("The answer to your query is", result4)
-
-# Calculation 5: 4200000 add 23090
-result5 = 4200000 + 23090
-print("The answer to your query is", result5)
-
-# Calculation 6: 4200000 subtract 7633
-result6 = 4200000 - 7633
-print("The answer to your query is", result6)
 
 # Print my name at the end
 print("Chris Alize")
