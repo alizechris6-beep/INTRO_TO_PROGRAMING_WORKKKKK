2fce1bbe6763a8cf3ea700ed8686ea9f7c87b34d
alizechris6-beep <alizechris6@gmail.com>
2026-03-08 21:07:48 -0400

    pp

---
+++ Flowchart & IPO Assignment.py
@@ -0,0 +1,18 @@
+# Movie price
+movie_price = 8
+
+# Money each person has
+harry_money = 7
+judy_money = 12
+
+# Check Harry
+if harry_money >= movie_price:
+    print("Harry will see the movie.")
+else:
+    print("Harry will go to the arcade.")
+
+# Check Judy
+if judy_money >= movie_price:
+    print("Judy will see the movie.")
+else:
+    print("Judy will go to the arcade.")
